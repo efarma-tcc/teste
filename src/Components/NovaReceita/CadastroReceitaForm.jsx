e6ef45cac8ea75b256281063160330ba42d43005
ltzLucas <ltzlucasteixeiralima@gmail.com>
2024-06-02 13:05:10 -0300

    Adicionando Botão apra Escolher um novo Remédio em Nova Receita

--- src/Components/NovaReceita/CadastroReceitaForm.jsx
+++ src/Components/NovaReceita/CadastroReceitaForm.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import Input from '../Forms/Input';
-import Button from '../Forms/Button';
+import InputLe from '../Forms/Input';
+import ButtonGreen from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import styles from './CadastroReceitaForm.module.css';
+import { Button } from '../ui/button'
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+import { PlusCircle} from 'lucide-react';
 
-const CadastroReceitaForm = () => {
+import { ComboBoxResponsive } from './ComboboxNovoRemeido';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+
+  const CadastroReceitaForm = () => {
+
     const nomePacienteR = useForm();
     const cpfR = useForm();
     const telefoneR = useForm();
@@ -14,6 +22,7 @@ const CadastroReceitaForm = () => {
     const assinatura = useForm();
     const dataPrescricao = useForm();
 
+
   return (
     <section className="animeLeft">
         <div className={styles.containerTituloRe}>
@@ -21,20 +30,58 @@ const CadastroReceitaForm = () => {
         </div>
         <form className={styles.formContainerRe}>
             <div className={styles.formrowRe}>
-                <Input label="Nome do Paciente" type="text" name="nomePacienteR" {...nomePacienteR}></Input>
-                <Input label="CPF do Paciente" type="number" name="cpfR" {...cpfR}></Input>
-                <Input label="Telefone do Paciente" type="number" name="telefoneR" {...telefoneR}></Input>
+                <InputLe label="Nome do Paciente" type="text" name="nomePacienteR" {...nomePacienteR}></InputLe>
+                <InputLe label="CPF do Paciente" type="number" name="cpfR" {...cpfR}></InputLe>
+                <InputLe label="Telefone do Paciente" type="number" name="telefoneR" {...telefoneR}></InputLe>
             </div>
             <div className={styles.formrowRe}>
-                <Input label="Nome do Médico" type="text" name="nomeMedico" {...nomeMedico}></Input>
-                <Input label="CRM do Médico" type="number" name="crm"></Input>
+                <InputLe label="Nome do Médico" type="text" name="nomeMedico" {...nomeMedico}></InputLe>
+                <InputLe label="CRM do Médico" type="number" name="crm"></InputLe>
             </div>
-            <Input label="Descrição da Receita" type="text-area" name="descricaoReceita" {...descricaoReceita}></Input>
+                <InputLe label="Descrição da Receita" type="text-area" name="descricaoReceita" {...descricaoReceita}></InputLe>
+
+            <div className='mb-5'>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="outline">
+                            <PlusCircle className='w-4 h-4 mr-2'/>
+                            Novo Item
+                        </Button>
+                    </DialogTrigger>
+
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Novo Item</DialogTitle>
+                            <DialogDescription>Escolha um remédio para adicionar a receita</DialogDescription>
+                        </DialogHeader>
+                        <ComboBoxResponsive></ComboBoxResponsive> 
+                        <div className='flex gap-5'>
+                            <div>
+                                <Label for="qtd">Quantidade</Label>
+                                <Input id='qtd' className="w-[100px]" />
+                            </div>
+                            <div >
+                                <Label for="descrição">Descrição</Label>
+                                <Input id='descrição' className="w-[350px]" />
+                            </div>
+                        </div>
+
+                        <DialogFooter>
+                            <DialogClose asChild>
+                                <Button type="button" variant="outline">Cancelar</Button>
+                            </DialogClose>
+                            <Button type="submit">Salvar</Button>
+                        </DialogFooter>
+                    </DialogContent>
+
+                </Dialog>
+
+            </div>
             <div className={styles.formrowRe}>
-                <Input label="Local" type="text" name="local" {...local}></Input>
-                <Input label="Data" type="date" name="dataPrescricao" {...dataPrescricao}></Input>
+                <InputLe label="Local" type="text" name="local" {...local}></InputLe>
+                <InputLe label="Data" type="date" name="dataPrescricao" {...dataPrescricao}></InputLe>
             </div>
-            <Button>Cadastrar nova Receita</Button>
+            <ButtonGreen>Cadastrar nova Receita</ButtonGreen>
         </form>
     </section>
   );
---
+++ src/Components/NovaReceita/ComboboxNovoRemeido.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+
+import { Button } from "../ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+
+const statuses = [
+  { value: "remedio1", label: "Remédio 01" },
+  { value: "remedio2", label: "Remédio 02" },
+  { value: "remedio3", label: "Remédio 03" },
+  { value: "remedio4", label: "Remédio 04" },
+  { value: "remedio5", label: "Remédio 05" },
+];
+
+export function ComboBoxResponsive() {
+  const [open, setOpen] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState(null);
+
+  useEffect(() => {
+    // Handle potential cleanup on component unmount (optional)
+    return () => {};
+  }, []);
+
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" className="w-[200px] justify-start">
+            {selectedStatus ? <>{selectedStatus.label}</> : <>Selecione o Remédio</>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0" align="start">
+          <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+        </PopoverContent>
+      </Popover>
+  );
+}
+
+function StatusList({ setOpen, setSelectedStatus }) {
+  return (
+    <Command>
+      <CommandInput placeholder="Filter status..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup>
+          {statuses.map((status) => (
+            <CommandItem
+              key={status.label}
+              value={status.label}
+              onSelect={(label) => {
+                setSelectedStatus(statuses.find((priority) => priority.label === label) || null);
+                setOpen(false);
+              }}
+            >
+              {status.label}
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+}
