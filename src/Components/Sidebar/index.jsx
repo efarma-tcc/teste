dd14b923f0c924d0d63286c261d9d4de7d0b5ac2
ltzLucas <ltzlucasteixeiralima@gmail.com>
2024-06-08 00:09:09 -0300

    arrumando erro collapse

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -2,19 +2,14 @@ import { Button, Layout, theme } from 'antd'
 import LogoSideBar from '../Logo-side-bar';
 import  style from "./index.module.css"
 import MenuList from '../Menu-list';
-import { useState } from 'react';
-import { RiMenuFold2Line, RiMenuUnfold2Line } from 'react-icons/ri';
 
 const {Sider} = Layout;
 
 export default function SidebarMenu(){
 
-    const [collapsed, setCollapsed] = useState(false)
-
     return(
             <>
                 <Sider 
-                    collapsed={collapsed} 
                     theme={'light'} 
                     className={style.sidebar}
                     trigger={null}
@@ -22,7 +17,6 @@ export default function SidebarMenu(){
                     <LogoSideBar path="src\Assets\redux.png" theme="ligth"/>
                    
                     <MenuList darkTheme={'ligth'} />
-
                 </Sider>
             </>
         )
